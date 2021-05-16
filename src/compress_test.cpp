74a42ac34fde2777c3a248826ef18729746ccef3
Marcelo Zimbres <mzimbres@gmail.com>
2021-05-16 13:59:07 +0200

    Fixes license.

--- src/compress_test.cpp
+++ src/compress_test.cpp
@@ -1,3 +1,21 @@
+/* Copyright (c) 2018-2021 Marcelo Zimbres Silva (mzimbres at gmail dot com)
+ *
+ * This file is part of smms.
+ *
+ * smms is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * smms is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with smms.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <string>
 #include <fstream>
 #include <stdexcept>
--- src/net.hpp
+++ src/net.hpp
@@ -1,19 +1,19 @@
-/* Copyright (c) 2018-2020 Marcelo Zimbres Silva (mzimbres at gmail dot com)
+/* Copyright (c) 2018-2021 Marcelo Zimbres Silva (mzimbres at gmail dot com)
  *
- * This file is part of tsvtree.
+ * This file is part of smms.
  *
- * tsvtree is free software: you can redistribute it and/or modify it
+ * smms is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * tsvtree is distributed in the hope that it will be useful, but
+ * smms is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with tsvtree.  If not, see <https://www.gnu.org/licenses/>.
+ * along with smms.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #pragma once
